936018880820e61003f3e544ac6b47e2538a2982
Débora Macêdo <deboramacedo886@gmail.com>
2022-11-07 22:12:29 -0300

    função fatorial js

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Calculadora_de_Mana
 projetinho pessoal
-# Link_Para_Testes
+## Link_Para_Testes
 https://raw.githack.com/kallyup/Calculadora_de_Mana/main/index.html
--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 function calcular() {
     console.log("CLICK!!!");
   
-    const ciclo = document.getElementById("ciclo_magia").value;
+    let ciclo = document.getElementById("ciclo_magia").value;
     const econ = document.getElementById("economizou").value;
     const aprim = document.getElementById("aprimoramento").value;
-    const resultado = Number(ciclo) - Number(econ) + Number(aprim) ;
+    const resultado = Number(cicloMagia(Number(ciclo))) - Number(econ) + Number(aprim) ;
     document.getElementById("custo").textContent = resultado;
     console.log(resultado);
 
     const mana = document.getElementById("mana_atual").value;
-    const sobrou = Number(mana)-resultado
+    const sobrou = Number(mana)-resultado;
     document.getElementById("sobra").textContent = sobrou;
+   // document.getElementById("mana_atual").value= sobrou;
+    
 }
+function cicloMagia(ciclo){
+    if (ciclo==1)
+    return 1;
+
+for (var i= ciclo-1; i>=1; i--){
+    ciclo+=i;
+ } 
+    return ciclo;
+}
+
